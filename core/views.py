c9c0943ba670adee44339550dc633c80e2904fcb
webcog <wajahatmurtaza144@gmail.com>
2023-12-07 20:43:31 +0500

    up

--- core/views.py
+++ core/views.py
@@ -3,7 +3,15 @@ from core.models import Category, Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    category = Category.objects.all().order_by("-id")
+    post = Post.objects.all().order_by("-id")
+
+    context = {
+        'category':category,
+        'post':post,
+    }
+
+    return render(request, 'index.html', context)
 
 def category(request):
     category = Category.objects.all()
