6484dd19c1305465bb11ed3b9e54c293e950dadc
webcog <wajahatmurtaza144@gmail.com>
2023-12-22 20:11:10 +0500

    22-12-2023

--- blog/urls.py
+++ blog/urls.py
@@ -18,11 +18,24 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path("accounts/password_reset/",
+          auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+          name='password_reset'),
+    path("accounts/password_reset/done/",
+         auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+         name='password_reset_done'),
+    path("accounts/reset/<uidb64>/<token>/",
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path("accounts/reset/done/",
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_confirm_done.html"),
+         name='password_reset_complete')
 
 ]
 if settings.DEBUG:
